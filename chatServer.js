5bef87ae3da8f47098564b45ebced5d664cd3623
Tassak <damian@mythe.pl>
2024-03-29 11:17:06 +0100

    Add broadcasting in chatServer

--- chatServer.js
+++ chatServer.js
@@ -1,25 +1,32 @@
 const net = require('net');
 
-// TODO: Broadcast
 const clients = [];
 
-const broadcast = (message, sender) => {};
+const broadcast = (message, sender) => {
+  clients.forEach((client) => {
+    if (client === sender) return;
+    client.write(message);
+  });
+};
 
 const server = net.createServer((socket) => {
-  console.log('Client connected.');
-
-  socket.write('Hello');
+  console.log('New client connected');
+  clients.push(socket);
+  socket.write('Welcome to the chat!\n');
 
   socket.on('data', (data) => {
-    console.log(`Data received from client: ${data}`);
-    socket.write(`Echo: ${data}`);
+    broadcast(data, socket);
   });
 
-  socket.on('end', () => console.log('Client disconnected.'));
-});
+  socket.on('end', () => {
+    console.log('Client disconnected');
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast('A client has left the chat.\n', socket);
+  });
 
-server.on('error', (err) => {
-  throw err;
+  socket.on('error', (err) => {
+    console.error(`Client error: ${err.message}`);
+  });
 });
 
 server.listen(3000, () => {
